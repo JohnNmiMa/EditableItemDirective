27212300be6d80df0442851919385003b693debe
John <johnmarksjr@gmail.com>
2014-09-12 16:51:45 -0600

    Done - added make-editable directive

--- app/app.js
+++ app/app.js
@@ -2,21 +2,33 @@ angular.module('editDirective', [])
 
 .controller('editDirectiveCtrl', ['$scope',
                 function($scope) {
-    $scope.btnMode = "Edit";
-
-    $scope.toggleEdit = function() {
-        if ($scope.btnMode == "Edit") {
-            $scope.btnMode = "Save";
-        } else {
-            $scope.btnMode = "Edit";
-        }
-    }
 }])
 
-.directive('editItem', function() {
+.directive('makeEditable', function() {
     return {
         restrict: 'A',
+        transclude: true,
+        templateUrl: 'make-editable-template.html',
+        replace: false,
         link: function($scope, element, attrs) {
+            // Get access to the button, so we can bind a click handler to it
+            var button = element.find('button.btn');
+            $scope.btnText = "Edit";
+
+            button.on('click', function() {
+                $scope.$apply(function() {
+                    console.log("The make-editable directive got a click!")
+                    toggleEdit();
+                })
+            });
+
+            toggleEdit = function() {
+                if ($scope.btnText == "Edit") {
+                    $scope.btnText = "Save";
+                } else {
+                    $scope.btnText = "Edit";
+                }
+            };
         }
     }
 });
